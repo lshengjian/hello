c78674275f100ee9e6a49aed3d6f1ebba7e0463e
lshengjian <lsj178@139.com>
2014-08-17 15:33:00 +0800

    prime

--- hello.go
+++ hello.go
@@ -1,7 +1,59 @@
 package main
-
-import "fmt"
-
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+//	"github.com/lshengjian/mymath"
+)
+func IsPrime(x int) bool {
+        for i := 2; i<=int(math.Sqrt(float64(x))); i++ {
+                if x%i==0 {
+                        return false
+                }
+        }
+        return true
+}
+func RemoveItem(data []int,idx int) []int {
+	return append(data[:idx], data[idx+1:]...)
+}
+/*func Filter(s []int, fn func(int) bool) []int {
+    var p []int 
+    for _, v := range s {
+        if fn(v) {
+            p = append(p, v)
+        }
+    }
+    return p
+}*/
+func Filter(s []int, idx int) []int {
+    p := s[0:idx+1]
+    d := s[idx]  
+    for i:=(idx+1);i<len(s);i++ {
+        if s[i]%d != 0 {
+            p = append(p, s[i])
+        }
+    }
+    return p
+}
 func main() {
-	fmt.Printf("Hello, world.\n")
+	argsWithoutProg := os.Args[1:]
+	TOTAL,_ := strconv.Atoi(argsWithoutProg[0])
+	data:= make([]int,TOTAL)
+	for i:=0 ;i<TOTAL;i++ {
+		data[i]=(i+2)
+	}
+	max := int( math.Sqrt( float64 ( data[len(data)-1] ) ) )+1
+	fmt.Println(data,max)
+	fmt.Println("=============")
+	idx := 0
+	for {
+		if data[idx] > max{
+            break
+		}
+		data = Filter(data , idx)
+		fmt.Println(data)
+		fmt.Println("------------------------")
+		idx++
+	}
 }
